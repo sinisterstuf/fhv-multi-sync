1c174a82248ca6e983ee6fd3bbb549ebb4b4696d
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-05-25 06:39:49 +0000

    Implemented logview.

--- trunk/MultiSync/src/at/fhv/multisync/ui/views/LogView.java
+++ trunk/MultiSync/src/at/fhv/multisync/ui/views/LogView.java
@@ -1,14 +1,33 @@
 package at.fhv.multisync.ui.views;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.part.ViewPart;
 
 public class LogView extends ViewPart {
+	private Table _logTable;
+
+	public LogView() {
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+		parent.setLayout(new FillLayout(SWT.HORIZONTAL));
 
+		_logTable = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
+		_logTable.setHeaderVisible(true);
+		_logTable.setLinesVisible(true);
+
+		TableColumn tblclmnFile = new TableColumn(_logTable, SWT.NONE);
+		tblclmnFile.setWidth(433);
+		tblclmnFile.setText("File");
+
+		TableColumn tblclmnDirection = new TableColumn(_logTable, SWT.NONE);
+		tblclmnDirection.setWidth(139);
+		tblclmnDirection.setText("Direction");
 	}
 
 	@Override
