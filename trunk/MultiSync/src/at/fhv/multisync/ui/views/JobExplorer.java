2699fe4003c5a0a75e1dbb3574ff81b1378bfac4
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-05-25 06:59:53 +0000

    Added tree view to job explorer

--- trunk/MultiSync/src/at/fhv/multisync/ui/views/JobExplorer.java
+++ trunk/MultiSync/src/at/fhv/multisync/ui/views/JobExplorer.java
@@ -1,12 +1,20 @@
 package at.fhv.multisync.ui.views;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.part.ViewPart;
 
 public class JobExplorer extends ViewPart {
+	public JobExplorer() {
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+		Tree _jobTree = new Tree(parent, SWT.BORDER);
 		// TODO Auto-generated method stub
 
 	}
