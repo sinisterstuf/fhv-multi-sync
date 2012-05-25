2b3a9f0b592a57bcfe57d0c6029a00e13575e746
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-05-25 07:16:39 +0000

    Implemented Jobeditor.

--- trunk/MultiSync/src/at/fhv/multisync/ui/editors/JobEditor.java
+++ trunk/MultiSync/src/at/fhv/multisync/ui/editors/JobEditor.java
@@ -1,13 +1,20 @@
 package at.fhv.multisync.ui.editors;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 
 public class JobEditor extends EditorPart {
+	public JobEditor() {
+	}
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
@@ -42,6 +49,19 @@ public class JobEditor extends EditorPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout(2, true));
+
+		Tree _masterDirTree = new Tree(parent, SWT.BORDER);
+		GridData gd__masterDirTree =
+				new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd__masterDirTree.widthHint = 274;
+		_masterDirTree.setLayoutData(gd__masterDirTree);
+
+		TabFolder _slaveDirTab = new TabFolder(parent, SWT.NONE);
+		GridData gd__slaveDirTab =
+				new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		gd__slaveDirTab.heightHint = 292;
+		_slaveDirTab.setLayoutData(gd__slaveDirTab);
 		// TODO Auto-generated method stub
 
 	}
