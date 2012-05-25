68690c3ddbe8cfbc43e90e8c9870f1b5741bd092
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-05-25 06:25:08 +0000

    Added empty view classes. Removed default view.

---
+++ MultiSync/src/at/fhv/multisync/ui/views/JobExplorer.java
@@ -0,0 +1,19 @@
+package at.fhv.multisync.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class JobExplorer extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+}
