8acf8ab76f8f3fb4075af38c97e61e64f0d70b16
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-01 08:18:26 +0000

    Added dummy jobs.

--- MultiSync/src/at/fhv/multisync/model/provider/tree/JobMockModel.java
+++ MultiSync/src/at/fhv/multisync/model/provider/tree/JobMockModel.java
@@ -3,6 +3,7 @@ package at.fhv.multisync.model.provider.tree;
 import java.util.ArrayList;
 import java.util.List;
 
+import at.fhv.multisync.model.Job;
 import at.fhv.multisync.model.JobGroup;
 import at.fhv.multisync.model.helper.SerializationHelper;
 
@@ -14,6 +15,17 @@ import at.fhv.multisync.model.helper.SerializationHelper;
  */
 public class JobMockModel {
 	private static ArrayList<JobGroup> JOBS;
+
+	static {
+		// FIXME for testing only
+		JobGroup group = new JobGroup();
+		Job j1 = new Job("Job 1");
+		j1.addSlave("C:\\tmp");
+		j1.setMaster("C:\\master");
+		group.addJob(j1);
+		JOBS = new ArrayList<JobGroup>();
+		JOBS.add(group);
+	}
 
 	/**
 	 * Load the jobs
