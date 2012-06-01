85dd1c3d08b27224532f94512395d357a57f4779
luukwullink@gmail.com <luukwullink@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-01 07:41:28 +0000

    changed action to command

--- MultiSync/src/at/fhv/multisync/ApplicationActionBarAdvisor.java
+++ MultiSync/src/at/fhv/multisync/ApplicationActionBarAdvisor.java
@@ -1,16 +1,21 @@
 package at.fhv.multisync;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.ICoolBarManager;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.action.ToolBarContributionItem;
 import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.swt.SWT;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
-
-import at.fhv.multisync.actions.MessagePopupAction;
 
 /**
  * An action bar advisor is responsible for creating, adding, and disposing of
@@ -23,13 +28,11 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	// them
 	// in the fill methods. This ensures that the actions aren't recreated
 	// when fillActionBars is called with FILL_PROXY.
-	private Action messagePopupAction;
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
 	}
 
-	@Override
 	protected void makeActions(final IWorkbenchWindow window) {
 		// Creates the actions and registers them.
 		// Registering is needed to ensure that key bindings work.
@@ -37,15 +40,5 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 		// file.
 		// Registering also provides automatic disposal of the actions when
 		// the window is closed.
-
-		messagePopupAction = new MessagePopupAction("Open Action", window);
-		register(messagePopupAction);
-	}
-
-	@Override
-	protected void fillCoolBar(ICoolBarManager coolBar) {
-		IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
-		coolBar.add(new ToolBarContributionItem(toolbar, "main"));
-		toolbar.add(messagePopupAction);
 	}
 }
