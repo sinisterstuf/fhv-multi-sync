e9a49b50fbc02cbf131cf9f5ae34ae4abbb67975
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-01 07:30:40 +0000

    Added product. Added sync log console.

--- trunk/MultiSync/src/at/fhv/multisync/Perspective.java
+++ trunk/MultiSync/src/at/fhv/multisync/Perspective.java
@@ -1,7 +1,13 @@
 package at.fhv.multisync;
 
+import java.io.PrintStream;
+
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 public class Perspective implements IPerspectiveFactory {
 
@@ -10,6 +16,15 @@ public class Perspective implements IPerspectiveFactory {
 		layout.setEditorAreaVisible(true);
 		layout.setFixed(true);
 
-	}
+		// initialize the console
+		MessageConsole console = new MessageConsole("SyncLog", null);
+		ConsolePlugin.getDefault().getConsoleManager()
+				.addConsoles(new IConsole[] { console });
 
+		// redirect output stream
+		final MessageConsoleStream stream = console.newMessageStream();
+		final PrintStream print = new PrintStream(stream);
+		System.setErr(print);
+		System.setOut(print);
+	}
 }
