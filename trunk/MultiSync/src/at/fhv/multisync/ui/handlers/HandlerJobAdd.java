2f7f7d31b3c69a321be25fa4761ed7c789e81584
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-06 13:38:03 +0000

    Adding a new job to a group is now possible.

--- trunk/MultiSync/src/at/fhv/multisync/ui/handlers/HandlerJobAdd.java
+++ trunk/MultiSync/src/at/fhv/multisync/ui/handlers/HandlerJobAdd.java
@@ -3,22 +3,52 @@ package at.fhv.multisync.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import at.fhv.multisync.ui.wizard.NewJobWizard;
+import at.fhv.multisync.model.Job;
+import at.fhv.multisync.model.JobGroup;
+import at.fhv.multisync.ui.dialog.StringInputDialog;
+import at.fhv.multisync.ui.views.JobExplorer;
 
+/**
+ * 
+ * The HandlerJobAdd class which handles job add commands.
+ * 
+ * @author Michael Sieber
+ */
 public class HandlerJobAdd extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// Instantiates and initializes the wizard
-		NewJobWizard wizard = new NewJobWizard();
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
+				.getActivePage().getSelection();
 
-		// Instantiates the wizard container with the wizard and opens it
-		WizardDialog dialog = new WizardDialog(
-				HandlerUtil.getActiveShell(event), wizard);
-		dialog.open();
+		Object selected = null;
+		// check if the selection is a tree selection
+		if (selection instanceof TreeSelection) {
+			selected = ((TreeSelection) selection).getFirstElement();
+		}
+
+		// check if a job group is selected
+		if (selected != null && selected instanceof JobGroup) {
+			StringInputDialog d = new StringInputDialog(PlatformUI
+					.getWorkbench().getActiveWorkbenchWindow().getShell(),
+					"Add Job", "Job name:");
+
+			if (d.open() == Window.OK) {
+				((JobGroup) selected).addJob(new Job(d.getInput()));
+				JobExplorer.refreshTree();
+			}
+			// if no group is selected warn the user
+		} else {
+			MessageDialog.openWarning(HandlerUtil.getActiveShell(event),
+					"Warning", "You have to select a job group first.");
+		}
 
 		return null;
 	}
