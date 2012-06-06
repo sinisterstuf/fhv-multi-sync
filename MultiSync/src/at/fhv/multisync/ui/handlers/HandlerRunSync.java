d6b0c8d7806cca44cf769ee7396b9c4420547be7
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-06 15:02:53 +0000

    Implemented run sync handler.

--- MultiSync/src/at/fhv/multisync/ui/handlers/HandlerRunSync.java
+++ MultiSync/src/at/fhv/multisync/ui/handlers/HandlerRunSync.java
@@ -1,21 +1,42 @@
 package at.fhv.multisync.ui.handlers;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import at.fhv.multisync.model.Job;
+import at.fhv.multisync.model.JobGroup;
+
+/**
+ * 
+ * The HandlerRunSync class which runs the selected group or job.
+ * 
+ * @author Michael Sieber
+ */
 public class HandlerRunSync extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		
-		
-		MessageDialog.openInformation(HandlerUtil.getActiveWorkbenchWindow(	event).getShell(), "Info", "Info for you");
-		
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
+				.getActivePage().getSelection();
+
+		Object selected = null;
+		// check if the selection is a tree selection
+		if (selection instanceof TreeSelection) {
+			selected = ((TreeSelection) selection).getFirstElement();
+		}
+
+		// check if a job group is selected
+		if (selected != null && selected instanceof JobGroup) {
+			((JobGroup) selected).runAll();
+			// check if a job is selected
+		} else if (selected != null && selected instanceof Job) {
+			((Job) selected).run();
+		}
+
 		return null;
 	}
 
