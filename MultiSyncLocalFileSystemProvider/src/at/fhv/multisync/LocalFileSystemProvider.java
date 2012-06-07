0b33de417f77c3f0950a91d66ddb5ad26826e88f
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-07 18:58:50 +0000

    Initial project import.

---
+++ MultiSyncLocalFileSystemProvider/src/at/fhv/multisync/LocalFileSystemProvider.java
@@ -0,0 +1,25 @@
+package at.fhv.multisync;
+
+import java.io.File;
+
+import at.fhv.multisync.bl.file.FileSystemProvider;
+
+/**
+ * 
+ * File system provider for local file system.
+ * 
+ * @author Michael Sieber
+ */
+public class LocalFileSystemProvider implements FileSystemProvider {
+	private final String _name = "Local File System";
+
+	@Override
+	public File[] getRoot() {
+		return File.listRoots();
+	}
+
+	@Override
+	public String toString() {
+		return _name;
+	}
+}
