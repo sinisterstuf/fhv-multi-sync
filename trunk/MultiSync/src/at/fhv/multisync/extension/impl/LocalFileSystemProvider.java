e5b38ecf4166ee055d4b8619e2748d0ae057277b
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-07 21:13:06 +0000

    Extension fix.

---
+++ trunk/MultiSync/src/at/fhv/multisync/extension/impl/LocalFileSystemProvider.java
@@ -0,0 +1,35 @@
+package at.fhv.multisync.extension.impl;
+
+import java.io.File;
+
+import at.fhv.multisync.extension.FileSystemProvider;
+
+/**
+ * 
+ * File system provider for local file system.
+ * 
+ * @author Michael Sieber
+ */
+public class LocalFileSystemProvider implements FileSystemProvider {
+	private final String _name = "Local File System";
+	private final String _matchingProtocol = "^[a-zA-Z]://$";
+
+	@Override
+	public File[] getRoot() {
+		return File.listRoots();
+	}
+
+	@Override
+	public String toString() {
+		return _name;
+	}
+
+	@Override
+	public boolean canHandle(String protocol) {
+		if (protocol == null || protocol.isEmpty()) {
+			return false;
+		}
+
+		return protocol.matches(_matchingProtocol);
+	}
+}
