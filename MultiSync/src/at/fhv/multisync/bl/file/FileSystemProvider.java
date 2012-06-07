541fcd63d4a4ea4e2c4d24e94012df2cc7727d38
msieber88@gmail.com <msieber88@gmail.com@9fbef7d9-f35d-cb75-476c-93c289381d65>
2012-06-07 20:07:06 +0000

    Added protocol check in extension point.

--- MultiSync/src/at/fhv/multisync/bl/file/FileSystemProvider.java
+++ MultiSync/src/at/fhv/multisync/bl/file/FileSystemProvider.java
@@ -17,4 +17,15 @@ public interface FileSystemProvider {
 	 * @return An array of all root folders
 	 */
 	public File[] getRoot();
+
+	/**
+	 * This method is used to determine if a protocol can be handled by the
+	 * plugin
+	 * 
+	 * @param protocol
+	 *            The protocol to check e.g. ftp:// (FTP Server) or C:// (Local
+	 *            Files)
+	 * @return True if the plugin can handle the protocol
+	 */
+	public boolean canHandle(String protocol);
 }
